9327ce4c5c3bb73461d800fd19499687211bf70f
Elfrian Triana <elfriantrianas@gmail.com>
2026-05-26 23:24:50 +0700

    chore: change Android minSdk to 30

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.aluna.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 33
+        minSdk = 30
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
